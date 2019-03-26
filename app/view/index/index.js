72f91b0b0681235660206348ad3204e5316973e8
chenminqun <chenminqun@batmobi.net>
2019-03-26 09:41:12 +0800

    hb

--- app/view/index/index.js
+++ app/view/index/index.js
@@ -1,8 +1,9 @@
 import './index.scss';
 import $ from 'jquery';
 const SOURCE = window.WEB_SOURCE || {};
+/** 开发环境 监听.njk 文件 */
 const tpl = SOURCE.isDev ? require('./index.njk') : undefined;
-
+/** 开发环境 监听.njk 文件 */
 $('#box').on('click', function() {
     $(this).html('1234');
 });
